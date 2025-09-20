fdef61b6b856224c40626de0747614f86a77ad24
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-20 13:34:42 +0100

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,20 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+
+# Serializer for Listing model
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        # Fields to include in API representation
+        fields = ['id', 'title', 'description', 'price_per_night', 'location', 'created_at']
+
+
+# Serializer for Booking model
+class BookingSerializer(serializers.ModelSerializer):
+    # Nested serializer to show listing details inside booking
+    listing = ListingSerializer(read_only=True)
+    
+    class Meta:
+        model = Booking
+        fields = ['id', 'listing', 'user', 'check_in', 'check_out', 'guests', 'created_at']
 
